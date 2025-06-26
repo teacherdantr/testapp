8ff71e932f03c2f710d205ffd7334daa2bd00282
DanTruong Huynh Ng <thetechdtr@gmail.com>
2025-06-26 12:58:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -114,15 +114,13 @@ SelectLabel.displayName = SelectPrimitive.Label.displayName
 const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children,value = "default", ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
-    value={value} // Ensure the value prop is passed
-
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
